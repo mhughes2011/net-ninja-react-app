059a228e409e1bdf36835a2055da86247a3e1a88
Max Hughes <mhughes2011@gmail.com>
2022-04-03 10:17:46 -0700

    started working with useState hook

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react'
 import './App.css';
 
 function App() {
+  let name = 'mario'
+  
+  const handleNameChange = () => {
+    name = 'luigi'
+    console.log(`The value of name is ${name}`)
+  }
+
   return (
-    <>
-      This is my first app for Net Ninja
-    </>
+    <div className='App'>
+      <h1>My name is {name}</h1>
+      <button onClick={handleNameChange}>Change Name</button>
+    </div>
   );
 }
 
