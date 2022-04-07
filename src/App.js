87efd32ee808cb25aafd791945b63965045712f2
Max Hughes <mhughes2011@gmail.com>
2022-04-07 16:40:09 -0700

    removing item from an array with filter

--- src/App.js
+++ src/App.js
@@ -2,28 +2,23 @@ import React, {useState} from 'react'
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('mario')
   const [events, setEvents] = useState([
     {title: 'marios birthday bash', id: 1},
     {title: 'bowsers live stream', id: 2},
     {title: 'race on moo moo farm', id: 3}
   ])
   
-  const handleNameChange = () => {
-    const userName = prompt('Give me your name...')
-    setName(userName)
-    console.log(`The value of name is ${userName}`)
+  const handleClick = (item) => {
+    setEvents(events.filter((e) => e.id !== item.id))
   }
 
   return (
     <div className='App'>
-      <h1>My name is {name}</h1>
-      <button onClick={handleNameChange}>Change Name</button>
-
       {/* You always get access to index when dealing with array methods */}
       {events.map((item, index) => (
         <div key={item.id}>
           <h2>{index} - {item.title}</h2>
+          <button onClick={() => handleClick(item)}>Delete Event</button>
         </div>
       ))}
     </div>
