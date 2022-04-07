5bd8ebe8aae504b00d679595cf63af8b235a951d
Max Hughes <mhughes2011@gmail.com>
2022-04-07 16:21:46 -0700

    learning array map method

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState('mario')
+  const [events, setEvents] = useState([
+    {title: 'marios birthday bash', id: 1},
+    {title: 'bowsers live stream', id: 2},
+    {title: 'race on moo moo farm', id: 3}
+  ])
   
   const handleNameChange = () => {
     const userName = prompt('Give me your name...')
@@ -14,6 +19,13 @@ function App() {
     <div className='App'>
       <h1>My name is {name}</h1>
       <button onClick={handleNameChange}>Change Name</button>
+
+      {/* You always get access to index when dealing with array methods */}
+      {events.map((item, index) => (
+        <div key={item.id}>
+          <h2>{index} - {item.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
