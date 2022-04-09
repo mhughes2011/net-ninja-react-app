ccabeb89c1b4e6b59f96d6edda670e18e7b21cf0
Max Hughes <mhughes2011@gmail.com>
2022-04-08 17:35:18 -0700

    learned conditional templates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true)
   const [events, setEvents] = useState([
     {title: 'marios birthday bash', id: 1},
     {title: 'bowsers live stream', id: 2},
@@ -14,8 +15,18 @@ function App() {
 
   return (
     <div className='App'>
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
       {/* You always get access to index when dealing with array methods */}
-      {events.map((item, index) => (
+      {showEvents && events.map((item, index) => (
         <div key={item.id}>
           <h2>{index} - {item.title}</h2>
           <button onClick={() => handleClick(item)}>Delete Event</button>
