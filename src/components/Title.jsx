145ddd043ff8fae30af080855fcb72b107dbbb8e
Max Hughes <mhughes2011@gmail.com>
2022-04-27 07:25:13 -0700

    refactor styling to be component specific

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import './Title.css'
 
 function Title({titleProp, subtitleProp}) {
   return (
-    <>
+    <div className='title-block'>
       <h1 className="title">{titleProp}</h1>
       <br />
       <h2 className="subtitle">{subtitleProp}</h2>
-    </>
+    </div>
   )
 }
 
