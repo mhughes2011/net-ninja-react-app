e8fc25053d6e5a1bd65e99aaed974cfba7129fff
Max Hughes <mhughes2011@gmail.com>
2022-04-27 07:30:06 -0700

    implemented inline styles

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,12 @@ import './Modal.css'
 function Modal(props) {
   return ReactDOM.createPortal((
     <div className='modal-backdrop'>
-      <div className="modal">
+    {/* The inline style tag below needs to curly brackets, the first set to notify of dynamic content and the second set for a JS object, like normal CSS */}
+      <div className="modal" style={{
+        border:'4px solid',
+        borderColor: '#ff4500',
+        textAlign: 'center'
+      }}>
         {props.children}
         <br />
         <button onClick={props.handleClose}>close</button>
